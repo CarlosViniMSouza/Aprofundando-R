9f7203cf5e4635e5046d3e33d2fa538b81d4f30f
CarlosViniMSouza <60679101+CarlosViniMSouza@users.noreply.github.com>
2021-04-12 19:31:51 -0300

    arquivo terminado

--- Pesquisa-Pessoal/LimpandoDados.R
+++ Pesquisa-Pessoal/LimpandoDados.R
@@ -17,7 +17,6 @@ library(tidyr)
 # Vamos explorar algumas funcionalidades :
 
 # 1 - Gather()
-# Usaremos dados disponíveis no R base para exemplificar:
 data("USArrests")
 str(USArrests)
 
@@ -30,7 +29,39 @@ head(USAlong)
 # adicionamos uma coluna 'tipo_crime' na tabela USArrests
 
 # Spread
-# É a operação antagônica do gather(). Ela espalha os valores de 
-# duas colunas em diversos campos !
 table2_alterada <- table2 %>% spread(key =type,value =count)
 head(table2_alterada)
+
+# Separate
+table3_alterada <-table3 %>% 
+separate(rate,into =c("cases","population"),sep='/')
+head(table3_alterada)
+
+# Unite
+table2.relatorio <- table2 %>% 
+    unite(type_year, type, year) %>% 
+        spread(key =type_year,value =count,sep ='_')
+table2.relatorio
+
+# Controlando Textos :
+a1 <-'apple'
+b2 <-'facebook'
+c3 <-'google'
+paste(a1, b2, c3)
+
+install.packages('stringr')
+library(stringr)
+
+#vamos usar o DB cnae para testar o pacote strigr :
+cnae.texto <-c('0 Elaboração do projeto',
+                '1 Elaboração do produto',
+                '2 Elaboração do marketing',
+                '3 Elaboração do pitch',
+                '4 Produção de todos as soluções',
+                '5 Produção finalizando',
+                '6 Projeto, produto, marketing e pitch prontos',
+                '7 Apresente o resultado')
+cnae <-str_sub(cnae.texto, 0, 2)
+texto <-str_sub(cnae.texto, 4)
+
+cnae
